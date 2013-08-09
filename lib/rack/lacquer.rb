7d7e1a292bfd398b44eec5abc067de05352e4ace
Adam Ross Cohen <a.ross.cohen@gmail.com>
2013-08-09 11:22:51 -0400

    initial commit

---
+++ lib/rack/lacquer.rb
@@ -0,0 +1,25 @@
+module Rack
+  class Lacquer
+    
+    def initialize app
+      @app = app
+    end
+
+    def call env
+      status, headers, response = @app.call env
+      response.map! { |part| process_includes part, env } if headers['Content-Type'] =~ /text\/html/
+      [status, headers, response]
+    end
+    
+    def process_includes part, env
+      document = Nokogiri::HTML part.to_s
+      document.css('include').each do |esi|
+        include_status, include_headers, include_response = @app.call env.merge 'PATH_INFO' => esi['src'], 'REQUEST_URI' => esi['src']
+        esi.after include_response.join
+        esi.remove
+      end
+      document.to_s
+    end
+    
+  end
+end
