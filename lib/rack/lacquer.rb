a3dfdc3bcd8949c8bee4de6072e1e04949418964
Adam Ross Cohen <a.ross.cohen@gmail.com>
2013-08-14 14:16:22 -0400

    needs to pass query params

--- lib/rack/lacquer.rb
+++ lib/rack/lacquer.rb
@@ -16,10 +16,12 @@ module Rack
     def process_includes part, env
       document = Nokogiri::HTML part.to_s
       document.css('include').each do |esi|
+        path_info, query_string = esi['src'].split('?')
         include_status, include_headers, include_response = @app.call env.merge({
-          'PATH_INFO' => esi['src'].split('?')[0],
-          'REQUEST_URI' => esi['src'].split('?')[0],
-          'QUERY_STRING' => esi['src'].split('?')[1]
+          'PATH_INFO' => path_info,
+          'REQUEST_URI' => esi['src'],
+          'QUERY_STRING' => query_string.to_s,
+          'action_dispatch.request.parameters' => env['action_dispatch.request.parameters'].merge!( Rack::Utils.parse_nested_query query_string )
         })
         esi.after include_response.join
         esi.remove
