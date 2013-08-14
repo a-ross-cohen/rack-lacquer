8ad6f5653dea6b378151452b0f69bfa6d7774533
Adam Ross Cohen <a.ross.cohen@gmail.com>
2013-08-14 13:55:57 -0400

    deal with query string

--- lib/rack/lacquer.rb
+++ lib/rack/lacquer.rb
@@ -9,14 +9,18 @@ module Rack
 
     def call env
       status, headers, response = @app.call env
-      response.map! { |part| process_includes part, env } if headers['Content-Type'] =~ /text\/html/ && response.respond_to?( :map! ) && status == 200
+      response.map! { |part| process_includes part, env } if headers['Content-Type'] =~ /text\/html/ && response.respond_to?( :map! )
       [status, headers, response]
     end
     
     def process_includes part, env
       document = Nokogiri::HTML part.to_s
       document.css('include').each do |esi|
-        include_status, include_headers, include_response = @app.call env.merge 'PATH_INFO' => esi['src'], 'REQUEST_URI' => esi['src']
+        include_status, include_headers, include_response = @app.call env.merge({
+          'PATH_INFO' => esi['src'].split('?')[0],
+          'REQUEST_URI' => esi['src'].split('?')[0],
+          'QUERY_STRING' => esi['src'].split('?')[1]
+        })
         esi.after include_response.join
         esi.remove
       end
